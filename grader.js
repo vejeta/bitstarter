3c06a742cdd8bb609e61faea726c6db0d7477201
Juan Manuel Méndez Rey <vejeta@gmail.com>
2013-07-11 00:02:59 +0200

    Updated checks.json and grader.js to support reading from an URL

--- grader.js
+++ grader.js
@@ -18,11 +18,14 @@ and basic DOM parsing.
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+
+ - https://github.com/danwrong/restler
 */
 
 var fs = require ('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,7 +47,22 @@ var loadChecks = function (checksfile){
 };
 
 var checkHtmlFile = function (htmlfile, checksfile){
+   out=checkHtmlStream(fs.readFileSync(htmlfile), checksfile)
+   return out;
+/*    
     $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks (checksfile).sort();
+    var out = {};
+    for (var ii in checks){
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+*/
+};
+
+var checkHtmlStream = function (htmlread, checksfile){
+    $ = cheerio.load(htmlread);
     var checks = loadChecks (checksfile).sort();
     var out = {};
     for (var ii in checks){
@@ -54,21 +72,50 @@ var checkHtmlFile = function (htmlfile, checksfile){
     return out;
 };
 
+
 var clone = function(fn){
     // Workaround for commander.js issue
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
+var buildfn = function(checks){
+    var readresponse = function (result, response){
+	if (result instanceof Error){
+	    console.error ('Error: ' + util.format (response.message));
+	}else{
+	    //htmlread=result;
+	    //console.error ("Leido: " + htmlread);
+	    var checkJson = checkHtmlStream (result, checks);
+	    var outJson = JSON.stringify (checkJson, null, 4);	
+	    console.log (outJson);
+
+	}
+    }
+    return readresponse;
+};
+
+var htmlread=".";
+
 if (require.main == module){
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <URL>', 'URL pointing to an html file. This option takes precedence over --file')
 	.parse(process.argv);
-    var checkJson = checkHtmlFile (program.file, program.checks);
-    var outJson = JSON.stringify (checkJson, null, 4);
-    console.log (outJson);
+    if (program.url){
+	var readresponse = buildfn(program.checks);
+	rest.get(program.url).on('complete',readresponse);
+	//console.log ("LEIDO : " + htmlread);
+	//var checkJson = checkHtmlStream (htmlread, program.checks);
+    }else{
+	var checkJson = checkHtmlFile (program.file, program.checks);
+	var outJson = JSON.stringify (checkJson, null, 4);	
+	console.log (outJson);
+    }
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
-
