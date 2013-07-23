d88154b2b5a67d63906ea93368e0912dd333dd33
Juan Manuel Méndez Rey <vejeta@gmail.com>
2013-07-23 06:35:48 +0200

    Imagenes y enlaces a redes sociales

--- web.js
+++ web.js
@@ -11,6 +11,11 @@ app.get('/', function(request, response) {
 
 });
 
+process.env.PWD = process.cwd();
+app.use('/media',express.static(process.env.PWD+'/media'));
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
